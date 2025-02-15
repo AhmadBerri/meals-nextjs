100fa2c7ca14542d5c04e2f2de138e711bc0059c
AhmadBerri <ahmadberri_1997@hotmail.com>
2025-02-15 15:04:12 +0200

    Fetch meal from the database, add error page, add not found page and loading state

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <mian className="error">
+      <h1>An error occurred!</h1>
+      <p> Failed to fetch meals data. Please try again later.</p>
+    </mian>
+  );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,13 @@
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -18,7 +25,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
