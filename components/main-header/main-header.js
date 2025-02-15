c591ea3aba1e05aaa05a21721d84deabc88d4b23
AhmadBerri <ahmadberri_1997@hotmail.com>
2025-02-15 14:14:33 +0200

    New Project NextJs project about Meals By Maximillian.

---
+++ components/main-header/main-header.js
@@ -0,0 +1,35 @@
+import logoImg from "@/assets/logo.png";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            width={500}
+            height={500}
+            priority
+            alt="A plate with food on it"
+          />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
